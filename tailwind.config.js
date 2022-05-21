749fefe5b687a20c6f148101a7613effd28f7ff4
mdgulamasib <gulam.asib@gmail.com>
2022-05-21 22:58:53 +0600

    environment setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        barlytheme: {
+          primary: "#ff7606",
+          secondary: "#7a2ea6",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
