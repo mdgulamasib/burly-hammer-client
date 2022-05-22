b590909757a62a914350b12fa4258081abe8b316
mdgulamasib <gulam.asib@gmail.com>
2022-05-22 14:54:32 +0600

    summary icon added

--- src/components/Home/ContactForm.js
+++ src/components/Home/ContactForm.js
@@ -2,31 +2,25 @@ import React from 'react';
 
 const ContactForm = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
-            <div class="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
 
-                <div class="text-center lg:text-left md:w-3/5">
-                    <h1 class="text-5xl font-bold">You are special!</h1>
-                    <p class="py-6">It is always a wise decison to leave us a message. We are very happy to assist you with valuable information you need. Feel free to send us a message.</p>
+                <div className="text-center lg:text-left md:w-3/5">
+                    <h1 className="text-5xl font-bold">You are special!</h1>
+                    <p className="py-6">It is always a wise decision to leave us a message. We are very happy to assist you with valuable information you need. Feel free to send us a message.</p>
                 </div>
 
-                <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div class="card-body">
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Contact Email</span>
-                            </label>
-                            <input type="email" placeholder="Email" required class="input input-bordered" />
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <div className="form-control">
+                            <input type="email" placeholder="Email" required className="input input-bordered" />
                         </div>
 
-                        <div class="form-control">
-                            <label class="label">
-                                <span class="label-text">Write Your Message</span>
-                            </label>
-                            <textarea type="text" class="input h-48 input-bordered" placeholder="Write Your Message" cols="30" rows="10"></textarea>
+                        <div className="form-control">
+                            <textarea type="text" className="input h-48 input-bordered" placeholder="Write Your Message" cols="30" rows="10"></textarea>
                         </div>
-                        <div class="form-control mt-6">
-                            <button class="btn btn-primary">Submit</button>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Submit</button>
                         </div>
                     </div>
                 </div>
--- src/components/Home/Summary.js
+++ src/components/Home/Summary.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import { useCountUp } from 'react-countup';
 import bgimg from '../../images/bgimg.gif'
+import rev from '../../images/summary/rev.png'
+import award from '../../images/summary/award.png'
+import global from '../../images/summary/world.png'
+import happy from '../../images/summary/happy.png'
 const Summary = () => {
 
     const countUpRef = React.useRef(null);
-    const { } = useCountUp({
+    const { dummy1 } = useCountUp({
         ref: countUpRef,
         start: 0,
         end: 89,
         delay: 0,
-        duration: 1,
+        duration: 3,
     });
     const countUpRef2 = React.useRef(null);
-    const { } = useCountUp({
+    const { dummy2 } = useCountUp({
         ref: countUpRef2,
         start: 0,
         end: 17,
         delay: 0,
-        duration: 1,
+        duration: 2,
     });
     const countUpRef3 = React.useRef(null);
-    const { } = useCountUp({
+    const { dummy3 } = useCountUp({
         ref: countUpRef3,
         start: 0,
-        end: 3000000,
+        end: 30,
         delay: 0,
-        duration: 5,
+        duration: 2,
     });
     const countUpRef4 = React.useRef(null);
-    const { } = useCountUp({
+    const { dummy4 } = useCountUp({
         ref: countUpRef4,
         start: 0,
         end: 3771,
         delay: 0,
-        duration: 2,
+        duration: 4,
     });
 
     return (
@@ -41,39 +45,42 @@ const Summary = () => {
 
 
             <div className='text-white md:flex gap-8 items-center'>
-                <div className='grid grid-cols-1 p-10 md:w-3/5'>
+                <div className='grid grid-cols-1 p-10 md:w-9/12'>
                     <h5 className='text-3xl font-bold text-primary'>Burly Corporation</h5>
                     <small className='font-bold'>Trusted heavy tools manufacturer</small>
                     <p className=' text-justify'>We are now one of the largest heavy tools manufacturer in the world supplying tools in more than 89 countries. We won 17 national and international Awards, and making 30M+ revenues every year. The thing made us proud is 3771+ global happy client in recent year.</p>
                 </div>
-                <div className='grid md:grid-cols-2 gap-4 px-10'>
+                <div className='grid md:grid-cols-2 gap-4 px-10 md:w-3/4'>
                     <div className='md:p-4 md:m-4 text-center'>
-                        <p className='text-3xl  font-bold text-primary text-center'>
-                            <span ref={countUpRef}></span>
+                        <img className='mx-auto' src={global} alt="rev" />
+                        <p className='text-2xl text-primary text-center'>
+
+                            <span ref={countUpRef}></span><span> +</span>
                         </p>
-                        <p className='text-xl  font-bold text-primary text-center uppercase'>Countries</p>
-                        <p>Business in 89 countries</p>
+                        <p className='text-md text-primary text-center uppercase'>Countries</p>
                     </div>
                     <div className='md:p-4 md:m-4 text-center'>
-                        <p className='text-3xl  font-bold text-primary text-center'>
+                        <img className='mx-auto' src={award} alt="rev" />
+                        <p className='text-2xl text-primary text-center'>
                             <span ref={countUpRef2}></span>
                         </p>
-                        <p className='text-xl  font-bold text-primary text-center uppercase'>Awards</p>
-                        <p>17 National and International Awards</p>
+                        <p className='text-md text-primary text-center uppercase'>Awards</p>
                     </div>
+
                     <div className='md:p-4 md:m-4 text-center'>
-                        <p className='text-3xl  font-bold text-primary text-center'>
-                            <span ref={countUpRef3}></span>
+                        <img className='mx-auto' src={rev} alt="rev" />
+                        <p className='text-2xl text-primary text-center'>
+                            <span ref={countUpRef3}></span><span>M +</span>
                         </p>
-                        <p className='text-xl  font-bold text-primary text-center uppercase'>Revenue</p>
-                        <p>30M+ Yearly Revenue</p>
+                        <p className='text-md text-primary text-center uppercase'>Yearly Revenue</p>
                     </div>
+
                     <div className='md:p-4 md:m-4 text-center'>
-                        <p className='text-3xl  font-bold text-primary text-center'>
-                            <span ref={countUpRef4}></span>
+                        <img className='mx-auto' src={happy} alt="rev" />
+                        <p className='text-2xl  text-primary text-center'>
+                            <span ref={countUpRef4}></span><span> +</span>
                         </p>
-                        <p className='text-xl  font-bold text-primary text-center uppercase'>Happy Clients</p>
-                        <p>3771+ Global happy client</p>
+                        <p className='text-md text-primary text-center uppercase'>Happy Clients</p>
                     </div>
                 </div>
             </div>
