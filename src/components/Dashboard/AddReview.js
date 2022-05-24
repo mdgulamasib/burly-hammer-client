5564ffb6f26445ebb36b1a599235840f6e7dd01d
mdgulamasib <gulam.asib@gmail.com>
2022-05-24 15:59:15 +0600

    add review full new code

--- src/components/Dashboard/AddReview.js
+++ src/components/Dashboard/AddReview.js
@@ -1,42 +1,16 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
+import { toast } from 'react-toastify';
 import auth from '../../firebase.init';
-import Loading from '../../components/Shared/Loading'
-
 
 const AddReview = () => {
-
-    const nameRef = useRef('')
-    const jobRef = useRef('');
-    const ratingsRef = useRef('');
-    const textRef = useRef('');
-    const imgRef = useRef('');
-
-
+    const { register, formState: { errors }, handleSubmit, reset } = useForm();
     const [user] = useAuthState(auth);
-    const [imgbb, setImgbb] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-    const [uploadedImg, setUploadedImg] = useState("");
     const imageStorageKey = '4afefea9112bd0a4e1d8c59a1753dbfb';
 
-
-
-    const uploadImage = (event) => {
-        setImgbb(event.target.files[0])
-    }
-
-    const handleAddReview = async (event) => {
-
-        setIsLoading(true)
-
-        event.preventDefault();
-        const name = nameRef.current.value;
-        const job = jobRef.current.value;
-        const ratings = ratingsRef.current.value;
-        const message = textRef.current.value;
-        const email = user.email;
-
-        const image = imgbb;
+    const onSubmit = async data => {
+        const image = data.image[0];
         const formData = new FormData();
         formData.append('image', image);
         const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
@@ -46,92 +20,145 @@ const AddReview = () => {
         })
             .then(res => res.json())
             .then(result => {
-                console.log(result)
                 if (result.success) {
-                    setUploadedImg(result.data.url);
+                    const img = result.data.url;
+                    const review = {
+                        name: data.name,
+                        email: user.email,
+                        job: data.job,
+                        ratings: data.specialty,
+                        text: data.text,
+                        img: img
+                    }
 
-                    const order = { name, email, job, ratings, message, uploadedImg };
+                    //sending to database
 
-                    // send data to insert orders
                     fetch('http://localhost:5000/reviews', {
                         method: 'POST',
                         headers: {
-                            'content-type': 'application/json'
+                            'content-type': 'application/json',
                         },
-                        body: JSON.stringify(order)
+                        body: JSON.stringify(review)
                     })
                         .then(res => res.json())
-                        .then(data => {
-                            console.log(data)
-                            if (data.acknowledged) {
-                                alert('Item added successfully!!!');
-                                event.target.reset();
-                                setIsLoading(false);
+                        .then(inserted => {
+                            if (inserted.insertedId) {
+                                toast.success('review added successfully')
+                                reset();
                             }
-
+                            else {
+                                toast.error('Failed to add the review');
+                            }
                         })
 
-
                 }
+
             })
-
-
     }
-
-    if (isLoading) {
-        return <Loading></Loading>
-    }
-
 
 
     return (
-        <div className='my-10 mx-auto'>
-            <form onSubmit={handleAddReview} className="md:w-72" >
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" for="username">
-                        Your Name
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" ref={nameRef} placeholder='Md Gulam Asib' required />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" for="job">
-                        Job Position
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="jobtitle" type="text" placeholder='CEO-at xxx company' ref={jobRef} required />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" for="rating">
-                        Ratings (1 -5)
-                    </label>
-                    <select className="shadow border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" ref={ratingsRef}>
-                        <option disabled defaultValue>Pick Your Ratings</option>
-                        <option value='1'>⭐(Worst)</option>
-                        <option value='2'>⭐⭐(Bad)</option>
-                        <option value='3'>⭐⭐⭐(Neutral)</option>
-                        <option value='4'>⭐⭐⭐⭐(Good)</option>
-                        <option value='5' selected>⭐⭐⭐⭐⭐(Awesome)</option>
-                    </select>
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" for="comment">
-                        Leave a Comment
-                    </label>
-                    <textarea className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="address" type="address" ref={textRef} placeholder="Tell everyone about our business relationship" required />
-                </div>
+        <div>
+            <h2 className="text-2xl font-bold text-center my-8 uppercase">Add a <span className='text-primary'>Review</span></h2>
+            <div className='flex justify-center'>
+                <form onSubmit={handleSubmit(onSubmit)}>
 
-                <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" for="job">
-                        Choose Your Photo
-                    </label>
-                    <input className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" id="img" type="file" ref={imgRef} onChange={uploadImage} required />
-                </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="block text-sm font-bold mb-2" for="username">
+                            Your Name
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Your Name"
+                            className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+                            {...register("name", {
+                                required: {
+                                    value: true,
+                                    message: 'Name is Required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                        </label>
+                    </div>
 
-                <div className="flex items-center">
-                    <button className="bg-primary uppercase hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >
-                        Submit Review
-                    </button>
-                </div>
-            </form>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="block text-sm font-bold" for="job">
+                            Job Position
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="CEO at XYZ"
+                            className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+                            {...register("job", {
+                                required: {
+                                    value: true,
+                                    message: 'Job Position is Required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.job?.type === 'required' && <span className="label-text-alt text-red-500">{errors.job.message}</span>}
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="block text-sm font-bold" for="comment">
+                            Leave a Comment
+                        </label>
+                        <textarea
+                            type="text"
+                            placeholder="Write your experience with us"
+                            className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+                            {...register("text", {
+                                required: {
+                                    value: true,
+                                    message: 'Text is Required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.text?.type === 'required' && <span className="label-text-alt text-red-500">{errors.text.message}</span>}
+                        </label>
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="block text-sm font-bold" for="rating">
+                            Ratings (1 -5)
+                        </label>
+                        <select {...register('specialty')} className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline" >
+                            <option disabled defaultValue>Pick Your Ratings</option>
+                            <option value='1'>⭐(Worst)</option>
+                            <option value='2'>⭐⭐(Bad)</option>
+                            <option value='3'>⭐⭐⭐(Neutral)</option>
+                            <option value='4'>⭐⭐⭐⭐(Good)</option>
+                            <option value='5' selected>⭐⭐⭐⭐⭐(Awesome)</option>
+                        </select>
+                    </div>
+
+                    <div className="form-control w-full max-w-xs">
+                        <label className="block text-sm font-bold " for="job">
+                            Choose Your Photo
+                        </label>
+                        <input
+                            type="file"
+                            className="shadow appearance-none border rounded w-full py-2 px-3  leading-tight focus:outline-none focus:shadow-outline"
+                            {...register("image", {
+                                required: {
+                                    value: true,
+                                    message: 'Image is Required'
+                                }
+                            })}
+                        />
+                        <label className="label">
+                            {errors.image?.type === 'required' && <span className="label-text-alt text-red-500">{errors.image.message}</span>}
+                        </label>
+                    </div>
+
+                    <input className='bg-primary uppercase cursor-pointer hover:bg-secondary text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type="submit" value="ADD REVIEW" />
+                </form>
+            </div>
         </div>
     );
 };
